16c408d8e489d438c014a5e12904f57feab61fa4
iBeni <ibeninobel05@gmail.com>
2025-03-03 22:36:04 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,100 @@
+<<<<<<< HEAD
+const textArea = document.getElementById("text_to_summarize");
+const submitButton = document.getElementById("submit-button");
+const summarizedText = document.getElementById("summary");
+
+submitButton.disabled = true;
+
+textArea.addEventListener("input", (e) => {
+    const textarea = e.target;
+
+    if (textarea.value.length > 200 && textarea.value.length < 100000) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
+
+submitButton.addEventListener("click", async () => {
+    // Start the spinning loading animation
+    submitButton.classList.add("submit-button--loading");
+    const text_to_summarize = textArea.value;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "text_to_summarize": text_to_summarize
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch('/summarize', requestOptions);
+        const summary = await response.text();
+
+        summarizedText.value = summary;
+
+    } catch (error) {
+        console.log('Error:', error.message);
+
+    } finally {
+        // Stop the spinning loading animation
+        submitButton.classList.remove("submit-button--loading");
+    }
+=======
+const textArea = document.getElementById("text_to_summarize");
+const submitButton = document.getElementById("submit-button");
+const summarizedText = document.getElementById("summary");
+
+submitButton.disabled = true;
+
+textArea.addEventListener("input", (e) => {
+    const textarea = e.target;
+
+    if (textarea.value.length > 200 && textarea.value.length < 100000) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
+
+submitButton.addEventListener("click", async () => {
+    // Start the spinning loading animation
+    submitButton.classList.add("submit-button--loading");
+    const text_to_summarize = textArea.value;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "text_to_summarize": text_to_summarize
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch('/summarize', requestOptions);
+        const summary = await response.text();
+
+        summarizedText.value = summary;
+
+    } catch (error) {
+        console.log('Error:', error.message);
+
+    } finally {
+        // Stop the spinning loading animation
+        submitButton.classList.remove("submit-button--loading");
+    }
+>>>>>>> b4c5c3b4dc20e2b373c5f7cb747e390fdb058d0a
+});

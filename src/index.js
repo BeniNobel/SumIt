03b1db3ccbfd58ba5db02d3c06bead3e7b38969f
iBeni <ibeninobel05@gmail.com>
2025-03-16 19:21:43 -0400

    fix: add start and dev scripts to package.json and clean up index.js, summarize.js, and HTML files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import dotenv from 'dotenv';
 // import bodyParser from 'body-parser';
@@ -32,39 +31,4 @@ app.post('/summarize', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
-=======
-import express from 'express';
-import dotenv from 'dotenv';
-// import bodyParser from 'body-parser';
-import summarize from './summarize.js';
-import cors from 'cors';
-
-dotenv.config();
-
-const app = express();
-const port = 3000;
-
-app.use(cors());
-
-app.use(express.static('public'));
-
-app.use(express.json());
-
-
-app.post('/summarize', async (req, res) => {
-    try {
-        const { text_to_summarize: text } = req.body;
-        const response = await summarize(text);
-
-        res.send(response);
-        
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('An error occurred while summarizing the text.');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
->>>>>>> b4c5c3b4dc20e2b373c5f7cb747e390fdb058d0a
 });
--- src/summarize.js
+++ src/summarize.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 // This is the function where the call to the API is made. Returns the summarized text as a string.
@@ -31,38 +30,4 @@ async function summarize(text) {
 }
 
 // Export the summarize function
-=======
-import axios from 'axios';
-
-// This is the function where the call to the API is made. Returns the summarized text as a string.
-async function summarize(text) {
-    let data = JSON.stringify({
-        "inputs": text,
-        "parameters": {
-          "max_length": 100,
-          "min_length": 30
-        }
-    });
-
-    let config = {
-        method: 'post',
-        url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + process.env.ACCESS_TOKEN
-        },
-        data: data
-      };
-
-    try {
-        const response = await axios.request(config);
-        return response.data[0].summary_text;
-    } catch (err) {
-        console.log(err);
-        throw new Error('An error occurred while summarizing the text.');
-    }
-}
-
-// Export the summarize function
->>>>>>> b4c5c3b4dc20e2b373c5f7cb747e390fdb058d0a
 export default summarize;
